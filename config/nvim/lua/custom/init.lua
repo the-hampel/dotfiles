2acd947dca33c5a021cd9ae7ee5999e97066804b
Alexander Hampel <ahampel@flatironinstitute.org>
2024-04-24 11:28:08 -0400

    activate rm whitespaces again

--- config/nvim/lua/custom/init.lua
+++ config/nvim/lua/custom/init.lua
@@ -1,7 +1,7 @@
 vim.opt.mouse = ""
 
--- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
---   pattern = { "*" },
---   command = [[%s/\s\+$//e]],
--- })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
 
