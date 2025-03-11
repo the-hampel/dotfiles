08248505eda98293f80101f9a1c913457e1adcbb
the-hampel <mail@alexander-hampel.de>
2025-03-11 12:22:59 +0100

    update

--- tools/env_vasp.sh
+++ tools/env_vasp.sh
@@ -15,12 +15,12 @@ while [[ $# -gt 0 ]]; do
             MODE=nvidia
             shift # move to next argument
             ;;
-        intel)
-            MODE=intel
+        intel24)
+            MODE=intel24
             shift # move to next argument
             ;;
-        intelgpu)
-            MODE=intelgpu
+        intel25)
+            MODE=intel25
             shift # move to next argument
             ;;
         --test)
@@ -39,31 +39,55 @@ if [ $MODE = gnu ]; then
     export FC=gfortran
     export CC=gcc
     export CXX=g++
+    export FFLAGS="-march=broadwell"
+    export CFLAGS="-march=broadwell"
+    export CXXFLAGS="-Wno-register -march=broadwell"
     export OMP_NUM_THREADS=1 
     export MKL_NUM_THREADS=1
     export MKL_INTERFACE_LAYER=GNU,LP64
     export MKL_THREADING_LAYER=SEQUENTIAL
 elif [ $MODE = nvidia ]; then
     module load vasp-nvhpc_mkl-dev/24.1_mkl-2023.2.0_ompi-4.1.6 gcc_system_8 profiling cross_platform openmp_support openacc_support cmake
-    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${NVROOT}/math_libs/lib64
+    export LD_LIBRARY_PATH=$NVROOT/cuda/12.3/lib64:$LD_LIBRARY_PATH
+    export LIBRARY_PATH=$NVROOT/cuda/12.3/lib64:$LIBRARY_PATH
+    export FC=nvfortran
+    export CC=nvc
+    export CXX=nvc++
+    export FFLAGS="-tp host --gcc-toolchain=${GCC_TOOLCHAIN}"
+    export CFLAGS="-tp host --gcc-toolchain=${GCC_TOOLCHAIN}"
+    export CXXFLAGS="-tp host --gcc-toolchain=${GCC_TOOLCHAIN}"
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${NVROOT}/cuda/12.3/lib64
     export OMP_NUM_THREADS=8
     export MKL_NUM_THREADS=1
-    export MKL_THREADING_LAYER=INTEL
-elif [ $MODE = intel ]; then
+    export MKL_INTERFACE_LAYER=PGI,LP64
+    export MKL_THREADING_LAYER=SEQUENTIAL
+    export BLA_VENDOR=Intel10_64lp_seq
+    # for RTX 4000 series
+    export CMAKE_CUDA_ARCHITECTURES=89
+    export VASP_CUDA_VERSION=11.8
+elif [ $MODE = intel24 ]; then
     module load vasp-intel-dev/2024.0.2_mkl-2023.2.0_impi-2021.10.0 impi-srun profiling cross_platform cmake
     export OMP_NUM_THREADS=1 
     export MKL_NUM_THREADS=1
+    export BLA_VENDOR=Intel10_64lp_seq
     export MKL_THREADING_LAYER=INTEL
     export FC=ifx
     export CC=icx
     export CXX=icpx
-elif [ $MODE = intelgpu ]; then
+    export FFLAGS="-xHOST"
+    export CFLAGS="-xHOST"
+    export CXXFLAGS="-xHOST"
+elif [ $MODE = intel25 ]; then
     export LC_ALL=C
     module load vasp-intel-dev/2025.0.3_mkl-2025.0.1_impi-2021.14.1 impi-srun profiling cross_platform cmake
     export FC=ifx
     export CC=icx
     export CXX=icpx
+    export FFLAGS="-xHOST"
+    export CFLAGS="-xHOST"
+    export CXXFLAGS="-xHOST"
     export MKL_THREADING_LAYER=INTEL
+    export BLA_VENDOR=Intel10_64lp_seq
     export OMP_NUM_THREADS=1
     export MKL_NUM_THREADS=1
     export I_MPI_DEBUG=1
--- tools/make_vasp_cmake.sh
+++ tools/make_vasp_cmake.sh
@@ -32,6 +32,10 @@ while [[ $# -gt 0 ]]; do
             MODE=vasp_ncl
             shift # move to next argument
             ;;
+        --cuda)
+            EXTRAS+=" -DVASP_CUDA=ON"
+            shift # move to next argument
+            ;;
         --test)
             TEST=true
             shift # move to next argument
@@ -49,7 +53,7 @@ while [[ $# -gt 0 ]]; do
           shift # move to next argument
           ;;
         --extras)
-          EXTRAS='-DVASP_HDF5=ON -DVASP_OPENMP=ON -DVASP_WANNIER90=ON'
+          EXTRAS="-DVASP_HDF5=ON -DVASP_OPENMP=ON -DVASP_WANNIER90=ON"
           shift # move to next argument
           ;;
         *)
